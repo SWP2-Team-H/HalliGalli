c93b2917e323c3b53d604714ffb78d03f5c30e9f
sinji2102 <sinzi031012@naver.com>
2022-11-20 22:29:24 +0900

    chore: bell.png 이미지 수정

--- display.py
+++ display.py
@@ -32,9 +32,9 @@ deck2 = pg.draw.rect(Display, BLACK, (width - (315 + 380), 505, 390, 230), 25)
 # 종
 bell = pg.image.load('src/bell.png')
 bell = pg.transform.scale(bell, (200, 200))
-Display.blit(bell, [(width/2)-100, 500])
+Display.blit(bell, [(width/2)-100, 520])
 
-# 좌측 카드 위치
+# 좌측 카드 위치 - 이 위치에 카드 튜플이나 리스트로 만들어서 받을 수 있게 해야 할 것 같아요
 banana1 = pg.image.load('src/banana1.png')
 banana1 = pg.transform.rotate(banana1, 270)
 Display.blit(banana1, [300, 500])
@@ -44,7 +44,7 @@ deck_l = pg.image.load('src/back_s.png')
 deck_l = pg.transform.rotate(deck_l, 270)
 Display.blit(deck_l, [0, 500])
 
-# 우측 카드 위치
+# 우측 카드 위치 - 이 위치에 카드 튜플이나 리스트로 만들어서 받을 수 있게 해야 할 것 같아요
 kiwi3 = pg.image.load('src/kiwi3.png')
 kiwi3 = pg.transform.rotate(kiwi3, 90)
 Display.blit(kiwi3, [width - (300 + 380) , 500])
