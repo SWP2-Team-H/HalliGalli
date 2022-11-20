c2b5d109b327dfa61320f591e6d6beb03617923e
sinji2102 <sinzi031012@naver.com>
2022-11-20 23:07:49 +0900

    feat: 글자 위치 설정

--- display.py
+++ display.py
@@ -20,37 +20,55 @@ Display = pg.display.set_mode(screensize, FULLSCREEN)
 Display.fill(WHITE) 
 
 # 글자 설정
-font = pg.font.SysFont("arial", 45, False, False)
-gamemode = font.render("게임 모드 : ", True, BLACK)
+font = pg.font.Font("src/font/EF_hyunygothic.ttf", 45)
 
+gamemode = font.render("게임 모드 : ", True, BLACK)
 Display.blit(gamemode, (100, 100))
+
+player1 = font.render("플레이어1 : ", True, BLACK)
+Display.blit(player1, (100, 200))
+
+p1_score = font.render("점수 : ", True, BLACK)
+Display.blit(p1_score, (100, 300))
+
+player2 = font.render("플레이어2 : ", True, BLACK)
+Display.blit(player2, (width - 700, 200))
+
+p2_score = font.render("점수 : ", True, BLACK)
+Display.blit(p2_score, (width - 700, 300))
+
+mc = font.render("사회자 : ", True, BLACK)
+Display.blit(mc, (100, height - 150))
+
+time = font.render("남은 시간 : ", True, BLACK)
+Display.blit(time, (width - 500, height - 150))
 
 # 덱 위치에 그림자 지게 하기
 deck1 = pg.draw.rect(Display, BLACK, (305, 505, 390, 230), 25)
 deck2 = pg.draw.rect(Display, BLACK, (width - (315 + 380), 505, 390, 230), 25)
 
 # 종
-bell = pg.image.load('src/bell.png')
+bell = pg.image.load('src/img/bell.png')
 bell = pg.transform.scale(bell, (200, 200))
 Display.blit(bell, [(width/2)-100, 520])
 
 # 좌측 카드 위치 - 이 위치에 카드 튜플이나 리스트로 만들어서 받을 수 있게 해야 할 것 같아요
-banana1 = pg.image.load('src/banana1.png')
+banana1 = pg.image.load('src/img/banana1.png')
 banana1 = pg.transform.rotate(banana1, 270)
 Display.blit(banana1, [300, 500])
 
 # 좌측 카드더미 위치
-deck_l = pg.image.load('src/back_s.png')
+deck_l = pg.image.load('src/img/back_s.png')
 deck_l = pg.transform.rotate(deck_l, 270)
 Display.blit(deck_l, [0, 500])
 
 # 우측 카드 위치 - 이 위치에 카드 튜플이나 리스트로 만들어서 받을 수 있게 해야 할 것 같아요
-kiwi3 = pg.image.load('src/kiwi3.png')
+kiwi3 = pg.image.load('src/img/kiwi3.png')
 kiwi3 = pg.transform.rotate(kiwi3, 90)
 Display.blit(kiwi3, [width - (300 + 380) , 500])
 
 # 좌측 카드더미 위치
-deck_r = pg.image.load('src/back_s.png')
+deck_r = pg.image.load('src/img/back_s.png')
 deck_r = pg.transform.rotate(deck_r, 90)
 Display.blit(deck_r, [width - 190, 500])
 
